856ad46be8c2aa254f2f607055db4d6edc09e82d
Wai Tong Chung <waitong94@gmail.com>
2022-10-12 14:35:19 -0700

    Appended bash files for fullh2 jung data

--- assets/bash/batch_download.sh
+++ assets/bash/batch_download.sh
@@ -4,3 +4,4 @@ kaggle datasets download -d waitongchung/inert-ch4o2-hit-dns
 kaggle datasets download -d waitongchung/round-jet-premixed-bfer
 kaggle datasets download -d waitongchung/round-jet-premixed-coffee
 kaggle datasets download -d waitongchung/half-lifted-flame-dns-li
+kaggle datasets download -d waitongchung/full-lifted-flame-dns-li
