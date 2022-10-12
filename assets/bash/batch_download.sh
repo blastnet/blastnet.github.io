47c01b8149a4fe9d74f621b391f8e8933179db04
Wai Tong Chung <waitong94@gmail.com>
2022-10-12 14:18:11 -0700

    Add batch download

---
+++ assets/bash/batch_download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kaggle datasets download -d waitongchung/inert-ch4o2-hit-dns
+kaggle datasets download -d waitongchung/round-jet-premixed-bfer
+kaggle datasets download -d waitongchung/round-jet-premixed-coffee
