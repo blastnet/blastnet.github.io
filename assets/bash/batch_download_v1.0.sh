222441efe99213371fa6d74ce75333b510cd376b
Wai Tong Chung <waitong94@gmail.com>
2023-06-13 10:29:00 -0700

    add bash files from prev versions

---
+++ assets/bash/batch_download_v1.0.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kaggle datasets download -d waitongchung/inert-ch4o2-hit-dns
+kaggle datasets download -d waitongchung/round-jet-premixed-bfer
+kaggle datasets download -d waitongchung/round-jet-premixed-coffee
+kaggle datasets download -d waitongchung/half-lifted-flame-dns-li
+kaggle datasets download -d waitongchung/full-lifted-flame-dns-li
---
+++ assets/bash/batch_download_v1.2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+kaggle datasets download -d waitongchung/inert-ch4o2-hit-dns
+kaggle datasets download -d waitongchung/round-jet-premixed-bfer
+kaggle datasets download -d waitongchung/round-jet-premixed-coffee
+kaggle datasets download -d waitongchung/half-lifted-flame-dns-li
+kaggle datasets download -d waitongchung/full-lifted-flame-dns-li
+kaggle datasets download -d waitongchung/premixed-flame-wall-ch4-air-dns-gri
+kaggle datasets download -d jguo96/transcritical-n2-channel-dns
+kaggle datasets download -d waitongchung/free-propagating-h2-vit-air-li-case-17
